8f33f451ed66610fafb05a8dd392e9f05c62935f
tdydcjovbi <githubpremium0@gmail.com>
2026-09-06 14:09:07 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -4,52 +4,43 @@ const fs = require("fs-extra");
 const moment = require("moment-timezone");
 
 module.exports.config = {
- name: "admin",
- version: "1.0.0",
- hasPermssion: 0,
- credits: "SHAHADAT SAHU",
- description: "Show Owner Info",
- commandCategory: "info",
- usages: "admin",
- cooldowns: 2
+    name: "admin",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "ULLASH", //don't change my credit 
+    description: "Show Owner Info",
+    commandCategory: "info",
+    usages: "",
+    cooldowns: 5
 };
 
 module.exports.run = async function({ api, event }) {
- const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
 
- const callback = () => api.sendMessage({
- body: `
-┌───────────────⭓
-│ 𝗢𝗪𝗡𝗘𝗥 𝗗𝗘𝗧𝗔𝗜𝗟𝗦
-├───────────────
-│👤 𝐍𝐚𝐦𝐞 : SHAHADAT SAHU
-│🚹 𝐆𝐞𝐧𝐝𝐞𝐫 : Maile
-│❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧 : Single
-│🎂 𝐀𝐠𝐞 : 18+
-│🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧 : Islam
-│🎓 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : HSC (2026)
-│🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬 : Khagrachori 
-└───────────────⭓
-
-┌───────────────⭓
-│ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
-├───────────────
-│📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸:
-│https://fb.com/Uhasbbz
-│💬 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽:
-│https://wa.me/01882333052
-└───────────────⭓
-
-┌───────────────⭓
-│ 🕒 𝗨𝗽𝗱𝗮𝘁𝗲𝗱 𝗧𝗶𝗺𝗲
-├───────────────
-│ ${time}
-└───────────────⭓
- `,
- attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
- }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
-
- return request("https://i.imgur.com/g3hlQ0Z.jpeg") //এখানে আপনার ছবির Imgur link বসাবেন✅
- .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
- .on('close', () => callback());
+    var callback = () => api.sendMessage({
+        body: `
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
+┣━━━━━━━━━━━━━━━━━━━━━┫
+┃ 👤 𝐍𝐚𝐦𝐞      : 𝐓𝐚𝐧𝐯𝐢𝐫
+┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
+┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐈𝐧 𝐂𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝
+┃ 🎂 𝐀𝐠𝐞       : 19+
+┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
+┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : লাড্ডু🙊😹
+┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : নরসিংদী.
+┣━━━━━━━━━━━━━━━━━━━━━┫
+┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : tanvir_hasan_6261
+┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : নাই থাকলেও দিমু না😏
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/profile.php?id=61577108993704
+┣━━━━━━━━━━━━━━━━━━━━━┫
+┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
+┗━━━━━━━━━━━━━━━━━━━━━┛
+        `,
+        attachment: fs.createReadStream(__dirname + "/cache/1.png")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
+  
+    return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
+        .on('close', () => callback());
 };
